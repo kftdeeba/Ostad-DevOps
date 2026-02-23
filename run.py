ca563f4e921f4ffddb297c0f61c6c421056947d4
kftasnim <kftdeeba2008@gmail.com>
2026-02-23 23:43:21 +0600

    Add all for vercel deployment

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 import os
-import sys
-from django.core.management import execute_from_command_line
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 
-# Set default DJANGO_SETTINGS_MODULE
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo_project.settings")  # <-- replace your_project_name
-
-# Get the port from Railway, default to 8000
-port = os.environ.get("PORT", 8000)
-
-# Run the server
-execute_from_command_line([sys.argv[0], "runserver", f"0.0.0.0:{port}"])
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
